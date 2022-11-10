b2fe8577df2b7466f69db101218319cf56214916
Fullstack-ITC <117898014+Fullstack-ITC@users.noreply.github.com>
2022-11-10 10:22:38 +0200

    Initial commit

---
+++ Exercises/2.js
@@ -0,0 +1,11 @@
+/**
+ * In the function, call the callback function
+ * only if callMe is true
+ * @param {boolean} callMe
+ * @param {Function} callback
+ */
+function callMeMaybe(callMe, callback) {
+  //
+}
+
+export default callMeMaybe;
